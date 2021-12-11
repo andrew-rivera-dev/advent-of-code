ea68c958ce487c3bf559b3f5e74eec8d5fd21ae6
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-12-10 23:42:25 -0500

    Day 1, Part 1 complete

--- day1.py
+++ day1.py
@@ -1,1 +1,18 @@
-# Solution for day 1
+# Day 1: How many measurements are larger than the previous measurement?
+
+# Part 1
+import fileinput
+
+nums = []
+count = 0
+
+for line in fileinput.input(files ='day1-data.txt'):
+    nums.append(int(line))
+
+for i in range(1, len(nums)):
+    if nums[i] > nums[i-1]:
+        count += 1
+
+print(count)
+
+# Part 2
