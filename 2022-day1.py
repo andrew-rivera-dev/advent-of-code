7998232de99e88abb5d347aefd095d211bfe2297
Andrew Rivera <andrewmarcrivera@gmail.com>
2022-12-01 01:30:38 -0500

    Day 1, 2022

---
+++ 2022-day1.py
@@ -0,0 +1,21 @@
+import fileinput
+from collections import deque
+
+biggest = [0,0,0]
+
+curr = 0
+
+for line in fileinput.input(files ='2022-day1-data.txt'):
+    if line == "\n":
+        small = min(biggest)
+        if curr > small:
+            i_small = biggest.index(small)
+            biggest[i_small] = curr
+
+        curr = 0
+
+        continue
+
+    curr += int(line)
+
+print(sum(biggest))
