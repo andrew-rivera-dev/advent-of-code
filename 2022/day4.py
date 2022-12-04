f0b0685e93d979b3a7dca216fb0fd8cbaf93375a
Andrew Rivera <andrewmarcrivera@gmail.com>
2022-12-04 00:47:17 -0500

    Day 4, part 2

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,19 +1,19 @@
 import fileinput
 
-def doesContain(range1, range2):
-    def contains(bigger, smaller):
-        return bigger[0] <= smaller [0] and bigger[1] >= smaller[1]
+def does_overlap(range1, range2):
+    s1 = set(range(range1[0], range1[1] + 1))
+    s2 = set(range(range2[0], range2[1] + 1))
 
-    return contains(range1, range2) or contains(range2, range1)
+    return len(s1.intersection(s2)) > 0
 
 total = 0
 
 for line in fileinput.input(files ='2022/day4-data.txt'):
-    a = line.rstrip().split(",")
-    b = list(map(lambda pair: pair.split("-"), a))
-    c = [list(map(int, x)) for x in b]
+    clean = line.rstrip().split(",")
+    split_hyphen = list(map(lambda pair: pair.split("-"), clean))
+    cast_int = [list(map(int, x)) for x in split_hyphen]
 
-    if doesContain(c[0], c[1]):
+    if does_overlap(cast_int[0], cast_int[1]):
         total += 1
 
 print(total)
