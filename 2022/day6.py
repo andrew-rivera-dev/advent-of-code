27b3d33dc8ae93a98aed3bc7c1f862f0e1058d96
Andrew Rivera <andrewmarcrivera@gmail.com>
2022-12-08 01:27:13 -0500

    Day 6, part 2

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 line = open('2022/day6-data.txt').readline()
 
-for i in range(4, len(line)):
-    marker = line[i-4:i]
+for i in range(14, len(line)):
+    marker = line[i-14:i]
     counter = Counter(marker)
     if all(counter[c] == 1 for c in marker):
         print(i)
